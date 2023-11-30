aafbe78ab60e735a0373b6c662f75e82d5383262
Miguel Martins <mfproencamartins@gmail.com>
2023-11-30 19:43:22 +0000

    unfinished

--- js/game.js
+++ js/game.js
@@ -1,3 +1,18 @@
 class Game {
-    // code to be added
+    constructor() {
+        this.startScreen = document.getElementById('game-intro')
+        this.gameScreen = document.getElementById('game-screen')
+        this.height = 800
+        this.width = 540
+        //this.player
+    }
+
+    start() {
+        this.startScreen.style.display = 'none'
+        this.gameScreen.style.display = 'block'
+        this.gameScreen.style.height = `${this.height}px`
+        this.gameScreen.style.width = `${this.width}px`
+
+        this.player = new Player(this.gameScreen)
+    }
 }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
-window.onload = function () {
-  const startButton = document.getElementById("start-button");
-  const restartButton = document.getElementById("restart-button");
+window.addEventListener('load', () => {
+  const startButton = document.getElementById("start-button")
+  const restartButton = document.getElementById("restart-button")
 
-  startButton.addEventListener("click", function () {
-    startGame();
-  });
+  const game = new Game()
 
   function startGame() {
-    console.log("start game");
+    console.log("start game")
+    game.start()
   }
-};
+  startButton.addEventListener("click", function () {
+    startGame()
+  })
+});
+
+  
+
+  
+
